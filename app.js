29603c00b7c167ca4ddb11583595000e83004cc0
tyckobra <tycko.bergman@elev.ga.ntig.se>
2023-01-13 11:05:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+});
+
+app.get('/', async function (req, res, next) {
+    res.render('index.njk', {
+        message: 'Hello world!',
+        title: 'Nunjucks hello world',
+    });
+});
---
+++ pm
@@ -0,0 +1,5 @@
+23-01-14
+Gjorde del 1,2 sen fastnade jag på del 3 på Övningar.
+Blev klurigt när man inte kunde kopiera rakt av, fick hjälp
+av Hjalmar.
+
