7ce4113748fd0a74891aa654af4b66ac61f615df
tyckobra <tycko.bergman@elev.ga.ntig.se>
2023-01-17 14:53:54 +0100

    23-01-17

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const  router = express.Router();
 
 router.get('/', async function (req, res, next) {
     res.render('index.njk', {
+        gringo: 'Gringo',
         message: 'Hello world! Now with a route file!',
         title: 'Nunjucks hello world',
-        gringo: 'Gringo'
     });
 });
 
